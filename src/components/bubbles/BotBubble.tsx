d4958e7ed22adfe46c0654643a61550578decd91
zeitderforschung <54367033+zeitderforschung@users.noreply.github.com>
2024-01-26 10:32:37 +0100

    fix mb

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -90,7 +90,7 @@ export const BotBubble = (props: Props) => {
   });
 
   return (
-    <div class="flex justify-start mb-4 items-start host-container" style={{ 'margin-right': '50px' }}>
+    <div class="flex justify-start mb-2 items-start host-container" style={{ 'margin-right': '50px' }}>
       <Show when={props.showAvatar}>
         <Avatar initialAvatarSrc={props.avatarSrc} />
       </Show>
--- src/components/bubbles/GuestBubble.tsx
+++ src/components/bubbles/GuestBubble.tsx
@@ -25,7 +25,7 @@ export const GuestBubble = (props: Props) => {
   });
 
   return (
-    <div class="flex justify-end mb-4 items-end guest-container" style={{ 'margin-left': '50px' }}>
+    <div class="flex justify-end mb-2 items-end guest-container" style={{ 'margin-left': '50px' }}>
       <span
         ref={userMessageEl}
         class="px-4 py-2 mr-2 max-w-full chatbot-guest-bubble prose"
